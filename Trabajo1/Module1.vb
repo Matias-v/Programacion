9b3d4c5716c253745df42a59994b74e58ecd06ea
Matias <matiasviera5925@gmail.com>
2020-04-03 15:52:58 -0300

    Le agregue la bibilioteca de clases

--- Trabajo1/Module1.vb
+++ Trabajo1/Module1.vb
@@ -2,9 +2,9 @@
 
     Sub Main()
 
-        Dim empleado1 As New Empleado(110, "Pepe", "Argento", "La casa de pepe", 1234, 30000.0, "administrador")
-        Dim empleado2 As New Empleado(111, "Damian", "Artigas", "La casa de Damian", 1222, 20000.0, "operario")
-        Dim empleado3 As New Empleado(100, "Marcelo", "Tinelli", "Showmatch", 2020, 40000.0, "gerente")
+        Dim empleado1 As New Logica.Empleado(110, "Pepe", "Argento", "La casa de pepe", 1234, 30000.0, "administrador")
+        Dim empleado2 As New Logica.Empleado(111, "Damian", "Artigas", "La casa de Damian", 1222, 20000.0, "operario")
+        Dim empleado3 As New Logica.Empleado(100, "Marcelo", "Tinelli", "Showmatch", 2020, 40000.0, "gerente")
         Dim op As Integer
         Dim totalA As Double
 
@@ -33,6 +33,7 @@
                     Console.WriteLine(empleado3.ToString())
                     Console.Write("Total a pagar:")
                     totalA = empleado1.CalculoSueldo() + empleado2.CalculoSueldo() + empleado3.CalculoSueldo()
+                    Console.WriteLine("-----------------------")
                     Console.WriteLine(totalA)
                     Console.WriteLine("-----------------------")
             End Select
